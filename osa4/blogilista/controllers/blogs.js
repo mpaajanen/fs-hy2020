13dfb5b10aeb18b343f5de4018a0d393b476ee44
mpaajanen <mikkopaa@gmail.com>
2020-12-16 16:06:17 +0200

    T4.17 tehty

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-vars */
 const blogsRouter = require('express').Router()
-const { response } = require('express')
-const { request } = require('../app')
+// const { response } = require('express')
+// const { request } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
     .find({})
-    .then(blogs => {
-      response.json(blogs)
-      // response.json(blogs.map(blog => blog.toJSON))
+    .populate('user', {
+      username: 1,
+      name: 1
     })
+
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
 // blogsRouter.post('/', (request, response) => {
@@ -26,14 +29,20 @@ blogsRouter.get('/', (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.json(savedBlog.toJSON())
 })
 
